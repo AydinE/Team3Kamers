3c6996813eb418444198252305795f4ed4ffefb2
Aydin Erdas <erdas001@gmail.com>
2017-09-21 13:54:09 +0200

    Added logger

--- src/main/java/KamerDemo.java
+++ src/main/java/KamerDemo.java
@@ -1,8 +1,15 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class KamerDemo {
 
     public static void main(String[] args) {
 
-        //Starting point of the application.
+        // Starting point of the application.
+
+        // Logging example
+        Logger logger = LogManager.getRootLogger();
+        logger.info("Hi");
 
     }
 
