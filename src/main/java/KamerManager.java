e19fdd93a45d49e78a474164d1ce4d58fac72ce7
Chaouki-Tiouassiouine <C.Tiouassiouine@Live.nl>
2017-09-22 14:29:31 +0200

    KamerManager List - ArrayList

--- src/main/java/KamerManager.java
+++ src/main/java/KamerManager.java
@@ -3,22 +3,5 @@ import java.util.List;
 
 public class KamerManager {
 
-    private List<Room> roomManager = new ArrayList<>();
-
-    private void add(){
-
-    }
-
-    private void change(){
-
-    }
-
-    private void block(){
-
-    }
-
-    private void delete(){
-
-    }
 
 }
--- src/main/java/RoomManager.java
+++ src/main/java/RoomManager.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class RoomManager {
+
+
+    private List<Room> roomManager = new ArrayList<>();
+
+    private void add(){
+
+    }
+
+    private void change(){
+
+    }
+
+    private void block(){
+
+    }
+
+    private void delete(){
+
+    }
 }
