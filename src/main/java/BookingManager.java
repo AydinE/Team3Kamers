1870b10a3d3d389edee6f03091aec9047057671c
trozie <twanrozie@gmail.com>
2017-09-22 15:05:41 +0200

    Added bookingManager

--- src/main/java/BookingManager.java
+++ src/main/java/BookingManager.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookingManager {
+
+    private List<Booking> bookingManager = new ArrayList<>();
+
+    private void overviewFreeRooms(){
+
+    }
+
+    private void searchingFreeRooms(){
+
+    }
+
+    private void availablilitySpecificRoom(){
+
+    }
 }
