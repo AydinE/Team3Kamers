696ac5372a3ed4690ad650e8d743ef4cda6e48c1
tijsstraatman <tijsstraatman@gmail.com>
2017-09-24 10:24:29 +0200

    klassen guest toegevoegd

--- src/main/java/GuestList.java
+++ src/main/java/GuestList.java
@@ -1,2 +1,43 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class GuestList {
+
+    private HashMap<String, Guest> guestList;
+
+    public GuestList() {
+        this.guestList = guestList;
+    }
+
+
+
+    public void searchGuest(int guestNumber){
+        //toevoegen method zoeken in lijst.
+
+    }
+
+    public HashMap<String, Guest> getGuestList() {
+        return guestList;
+    }
+
+    public void addGuest(String s, Guest guest){
+        guestList.put(s ,guest);
+    }
+
+
+    public void alterGuestAddress(Guest guest, String address){  // guest moet nog toegevoegd worden. String is nu key.
+        guest.getAddress();
+        guest.setAddress(address);                                            // op deze regel roepen we de setter aan.
+        guestList.put(guest.getFirstName() + ' '+ guest.getLastName(), guest);
+
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "GuestList{" +
+                "guestList=" + guestList +
+                '}';
+    }
 }
--- src/main/java/KamerDemo.java
+++ src/main/java/KamerDemo.java
@@ -1,15 +1,42 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.HashMap;
+
 public class KamerDemo {
 
     public static void main(String[] args) {
 
         // Starting point of the application.
 
+
+
+        Guest guest = new Guest(1, "Tijs","Straatman",
+                "straat1","d","Sleeuwijk","Nedelrand","06123",
+                "tstraatman@gmail.com");
+
+        GuestList guestList = new GuestList();
+
+        HashMap<String, Guest> hList = guestList.getGuestList();
+
+        guest.getLastName();
+        guest.getAddress();
+        guest.getFirstName();
+        guest.getCity();
+        guest.getCountry();
+        guest.getEmail();
+        guest.getGuestNumber();
+        guest.getPhoneNumber();
+        guest.getPostalCode();
+
+        guestList.addGuest("Tijs"+ ' '+ "Straatman",guest);
+
+        System.out.println(guestList);
+
+
         // Logging example
-        Logger logger = LogManager.getRootLogger();
-        logger.info("Hi");
+//        Logger logger = LogManager.getRootLogger();
+//        logger.info("Hi");
 
     }
 
