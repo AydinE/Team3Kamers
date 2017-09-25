02d943e9edd40b009dbb4dc1268cb53cddd25509
trozie <twanrozie@gmail.com>
2017-09-25 15:53:19 +0200

    model room.java error's gefixt.

--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -10,13 +10,13 @@ public class Room {
     private int roomNr;
     private RoomType typeOfRoom;
     private RoomSize sizeOfRoom;
-    private LocalDateTime availableFrom;
+    private LocalDateTime createdOn;
 
-    public Room(int identifier, RoomType typeOfRoom, RoomSize sizeOfRoom, LocalDateTime availableFrom) {
+    public Room(int identifier, RoomType typeOfRoom, RoomSize sizeOfRoom, LocalDateTime createdOn) {
         roomNr = identifier;
         this.typeOfRoom = typeOfRoom;
         this.sizeOfRoom = sizeOfRoom;
-        this.availableFrom = availableFrom;
+        this.createdOn = createdOn;
 
     }
 
@@ -32,7 +32,7 @@ public class Room {
         return sizeOfRoom;
     }
 
-    public LocalDateTime getAvailableFrom() {
-        return availableFrom;
+    public LocalDateTime getCreatedOn() {
+        return createdOn;
     }
 }
