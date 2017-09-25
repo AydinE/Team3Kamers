54e56ada3fe5fa1ad6615d714ceeac096c019d78
Chaouki-Tiouassiouine <C.Tiouassiouine@Live.nl>
2017-09-25 15:30:18 +0200

    RoomController - Added Methods

--- src/main/java/RoomManager.java
+++ src/main/java/RoomManager.java
@@ -1,26 +1,3 @@
 import model.Room;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class RoomManager {
-
-
-    public List<Room> roomManager = new ArrayList<>();
-
-    public void add(){
-
-    }
-
-    public void change(){
-
-    }
-
-    public void block(){
-
-    }
-
-    public void delete(){
-
-    }
-}
--- src/main/java/controller/RoomController.java
+++ src/main/java/controller/RoomController.java
@@ -1,4 +1,20 @@
 package controller;
 
 public class RoomController {
+
+    public void add(){
+
+    }
+
+    public void change(){
+
+    }
+
+    public void block(){
+
+    }
+
+    public void delete(){
+
+    }
 }
