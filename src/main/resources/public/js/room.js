483d97f15d9ed42b7d0493525c75a17fb0cb2468
Emielvr <emielvr@hotmail.com>
2017-09-26 15:04:11 +0200

    Reading json object client side

--- src/main/resources/public/js/room.js
+++ src/main/resources/public/js/room.js
@@ -4,8 +4,7 @@ $("#createButton").click(function() {
     var roomSize = $("#roomSize").val();
     var url = "/api/addRoom?roomNr=" + roomNumber + "&roomType=" + roomType + "&roomSize=" + roomSize;
     $.get(url, function(data) {
-        var json = JSON.stringify(data);
-        console.log(json);
-        $("#outputField").html("Room created")
+        console.log(data);
+        $("#outputField").html("Room " + data.roomNr + " created")
     });
 });
