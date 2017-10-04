16987c04bb17edc3213b41554d5dd242282ce4e1
Chaouki-Tiouassiouine <C.Tiouassiouine@Live.nl>
2017-10-04 15:53:25 +0200

    Booking and Room model added the annotations, Pom file added dependencies. modified the Repository Room and Booking

--- src/main/java/com/capgemini/controller/BookingController.java
+++ src/main/java/com/capgemini/controller/BookingController.java
@@ -14,7 +14,6 @@ public class BookingController {
 
     public BookingController(){
 
-
     }
 
     @RequestMapping(value = "/addBooking", method = RequestMethod.POST)
--- src/main/java/com/capgemini/model/Booking.java
+++ src/main/java/com/capgemini/model/Booking.java
@@ -1,9 +1,20 @@
 package com.capgemini.model;
 
+import com.sun.javafx.beans.IDProperty;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@Entity
 public class Booking {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
     private int bookingNr;
     private Guest guest;
     private Room room;
@@ -12,6 +23,20 @@ public class Booking {
     private boolean checkedIn;
 
     public Booking() {}
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id=" + id +
+                ", bookingNr=" + bookingNr +
+                ", guest=" + guest +
+                ", room=" + room +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", checkedIn=" + checkedIn +
+                '}';
+    }
+
 
     public int getBookingNr() {
         return bookingNr;
