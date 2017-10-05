170bbb74ebd8b681607fe926f8f6e0424f2f350a
Emielvr <emielvr@hotmail.com>
2017-10-05 11:07:16 +0200

    Finished version of sidebar

--- src/main/resources/public/js/sidebar.js
+++ src/main/resources/public/js/sidebar.js
@@ -10,27 +10,47 @@ function clickTab(tab) {
     // If this tab is already selected
     if (tab.hasClass("selected")) {
         tab.toggleClass("selected");
-        $('#sidebar').toggleClass("inactive");
         $("#sidebar-content").toggle();
+        $('#sidebar').toggleClass("inactive");
         return;
     }
+    console.log(tab);
+    changeTab(tab);
+ }
 
-    var flag = false;
-    // Find another tab that is selected
-    $("#sidebar-icons li div").each(function(k, v) {
-        if ($(v).hasClass("selected")) {
-            $(v).toggleClass("selected");
-            tab.toggleClass("selected");
-            flag = true;
-            return;
-        }
-    });
-    if (flag) {
-        return;
+ function changeTab(tab) {
+     var flag = false;
+     // Find another tab that is selected
+     $("#sidebar-icons li div").each(function(k, v) {
+         if ($(v).hasClass("selected")) {
+             $(v).toggleClass("selected");
+             toggleContentVisibility($(v));
+             tab.toggleClass("selected");
+             toggleContentVisibility(tab);
+             flag = true;
+             return;
+         }
+     });
+     if (flag) {
+         return;
+     }
+
+     // No tabs selected
+     tab.toggleClass("selected");
+     $("#sidebar-content").toggle();
+     $('#sidebar').toggleClass("inactive");
+ }
+
+ function toggleContentVisibility(tab) {
+    if (tab.attr("id") == "search") {
+        $("#sidebar-content-search").show();
+        $("#sidebar-content-add").hide();
+        console.log("Showing-search");
+    } else if (tab.attr("id") == "add") {
+        $("#sidebar-content-add").show();
+        $("#sidebar-content-search").hide();
+        console.log("Showing add");
     }
-
-    // No tabs selected
-    tab.toggleClass("selected");
-    $('#sidebar').toggleClass("inactive");
-    $("#sidebar-content").toggle();
+        console.log($("#sidebar-content-search"));
+        console.log($("#sidebar-content"));
  }
