d66a4f4884f5a2e07286820b0bdbeca98589f6b1
Aydin Erdas <1353136erdas@zuyd.nl>
2017-10-05 23:26:54 +0200

    Bijna gefixed

--- src/main/resources/public/js/gettasks.js
+++ src/main/resources/public/js/gettasks.js
@@ -1,24 +1,84 @@
-jQuery.fn.GetTasks = function() {
+$(document).ready(function() {
+
+    // Tasks - >      Bookings (Name)
+    // Users - >      Rooms
+    // User tasks - > Booking (dates)
+
+    console.log("Gettasks was called");
 
     var tasks = [];
+    var users = [];
+    //var userTasks = [];
 
+    // Get Tasks
     var url = "/api/getAllBookings";
     $.get(url, function(data) {
 
-            for (i = 0; i < data.length; i++) {
+        for (i = 0; i < data.length; i++) {
 
-                var obj = {
-                    id: data[i].bookingNr,
-                    name: data[i].guest.firstName + " " + data[i].guest.lastName,
-                    description: data[i].startDate + " " + data[i].endDate,
-                    color: '#536DFE'
-                }
-
-                tasks.push(obj);
+            var taskObj = {
+                //id: "room" + data[i].bookingNr,
+                id: data[i].bookingNr,
+                name: data[i].guest.firstName + " " + data[i].guest.lastName,
+                description: data[i].startDate + " - " + data[i].endDate,
+                color: '#536DFE'
             }
+            tasks.push(taskObj);
+        }
 
     });
 
-    return tasks;
+    // Get Users
+    var url = "/api/getAllRooms";
+    $.get(url, function(data) {
 
-};
+        for (i = 0; i < data.length; i++) {
+
+            var roomObj = {
+                name: data[i].roomNr,
+                group: 'Rooms',
+                tasks: []
+            }
+            users.push(roomObj);
+        }
+
+    });
+
+    // Get UserTasks
+    var url = "/api/getAllBookings";
+    $.get(url, function(data) {
+
+        for (i = 0; i < data.length; i++) {
+
+            console.log("length " + data[i].startDate[1]);
+
+            if (data[i].startDate[1] < 10) {
+                String.fromCharCode(48) + String.fromCharCode(data[i].startDate[1] + 49);
+            }
+
+            if (data[i].startDate[2] < 10) {
+                String.fromCharCode(48) + String.fromCharCode(data[i].startDate[2] + 49);
+            }
+
+            var taskObj = {
+                id: data[i].room.roomNr,
+                start_date: data[i].startDate[0] + "-" + data[i].startDate[1] + "-" + data[i].startDate[2] + " " + "17:00",
+                end_date: data[i].endDate[0] + "-" + data[i].endDate[1] + "-" + data[i].endDate[2] + " " + "11:00"
+            }
+            //tasks.push(taskObj);
+            for (i = 0; i < users.length; i++) {
+                if (users[i].name == taskObj.id) {
+                    users[i].tasks.push(taskObj);
+                }
+            }
+
+        }
+
+    });
+
+    console.log(tasks);
+    console.log(users);
+
+    $().schedulerInit(tasks, users)
+
+});
--- src/main/resources/public/js/schedulerinit.js
+++ src/main/resources/public/js/schedulerinit.js
@@ -1,11 +1,22 @@
-$(document).ready(function () {
-        $("#pit-scheduler").pitScheduler({
+jQuery.fn.schedulerInit = function(taskies, usies) {
+
+        //var MyFunctionResult = $().GetTasks();
+        console.log("scheudlerinit called");
+        console.log(taskies);
+        console.log(usies);
+
+        var tasken = taskies;
+        var usen = usies;
+
+        //console.log(MyFunctionResult);
+
+        var pitObj = {
             locale: 'en',
             defaultDisplay: 'months',
             hideEmptyLines: true,
             disableLabelsMovement: false,
             defaultGroupName: 'Default group',
-            defaultDate: '2016-09-01 16:30',
+            defaultDate: '2017-10-01 16:30',
             disableNotifications: false,
             notificationDuration: 4000,
             hideSpinner: true,
@@ -19,152 +30,41 @@ $(document).ready(function () {
             onUserTaskDeletion: '',
             onTaskEdition: '',
             disableUndo: false,
-            resizeTask: true,
-            tasks:  [
-                {
-                    id: 'djJ3d7sjk928S0',
-                    name: 'Jacuzzi access',
-                    description: 'Allows users to access to the jacuzzi',
-                    color: '#fcd720',
-                    tag: 'limited places',
-                    tagColor: '#50d371'
-                },
-                {
-                    id: 'fJD67Bd4jh7',
-                    name: 'SPA access',
-                    description: 'Allows usesr to access to the SPA',
-                    color: '#E91E63'
-                },
-                {
-                    id: 'dj8aA9nL0Su7',
-                    name: 'Internet access',
-                    description: 'Allows users to access to internet',
-                    color: '#536DFE'
-                },
-                {
-                    id: 'fLKd67zPkj',
-                    name: 'VIP services',
-                    color: '#50d371',
-                    tag: 'Only VIPs',
-                    tagColor: '#d200a5'
-                }
-            ],
-            users: [
-                   {
-                    name: 'Ai WeiWei',
-                    group: 'Group A',
-                    tasks: [
-                        {
-                            id: 'djJ3d7sjk928S0',
-                            start_date: '2015-09-10 00:00',
-                            end_date: '2016-09-20 00:00'
-                        },
-                        {
-                            id: 'djJ3d7sjk928S0',
-                            start_date: '2016-09-28 6:00',
-                            end_date: '2017-09-28 18:30'
-                        },
-                        {
-                            id: 'fJD67Bd4jh7',
-                            start_date: '2016-09-08 10:40',
-                            end_date: '2016-09-21 16:00'
-                        },
-                        {
-                            id: 'fJD67Bd4jh7',
-                            start_date: '2016-09-23 10:10',
-                            end_date: '2016-09-29 16:50'
-                        },
-                        {
-                            id: 'fLKd67zPkj',
-                            start_date: '2016-09-10 08:00',
-                            end_date: '2016-09-12 20:00'
-                        }
-                    ]
-                },
-                {
-                    name: 'Michel Petrucciani',
-                    group: 'Group A',
-                    tasks: [
-                        {
-                            id: 'djJ3d7sjk928S0',
-                            start_date: '2016-09-01 06:00',
-                            end_date: '2016-09-04 00:00'
-                        },
-                        {
-                            id: 'djJ3d7sjk928S0',
-                            start_date: '2016-09-04 11:00',
-                            end_date: '2016-09-08 18:00'
-                        },
-                        {
-                            id: 'fLKd67zPkj',
-                            start_date: '2016-09-07 17:00',
-                            end_date: '2016-09-11 20:00'
-                        }
-                    ]
-                },
-                {
-                    name: 'Jimi Hendrix',
-                    group: 'Group B',
-                    tasks: [
-                        {
-                            id: 'djJ3d7sjk928S0',
-                            start_date: '2016-09-01 02:30',
-                            end_date: '2016-09-03 09:00'
-                        },
-                        {
-                            id: 'djJ3d7sjk928S0',
-                            start_date: '2016-09-04 17:30',
-                            end_date: '2016-09-24 07:42'
-                        },
-                        {
-                            id: 'dj8aA9nL0Su7',
-                            start_date: '2016-09-02 17:30',
-                            end_date: '2016-09-11 07:42'
-                        },
-                        {
-                            id: 'fJD67Bd4jh7',
-                            start_date: '2016-09-13 10:00',
-                            end_date: '2016-09-26 16:00'
-                        },
-                        {
-                            id: 'fJD67Bd4jh7',
-                            start_date: '2016-09-30 10:00',
-                            end_date: '2016-10-05 16:00'
-                        },
-                        {
-                            id: 'fJD67Bd4jh7',
-                            start_date: '2016-10-23 10:00',
-                            end_date: '2016-11-04 16:00'
-                        },
-                        {
-                            id: 'fJD67Bd4jh7',
-                            start_date: '2016-11-06 10:00',
-                            end_date: '2016-11-09 16:00'
-                        },
-                        {
-                            id: 'fJD67Bd4jh7',
-                            start_date: '2016-11-10 10:00',
-                            end_date: '2016-11-21 16:00'
-                        },
-                        {
-                            id: 'fLKd67zPkj',
-                            start_date: '2016-09-01 08:00',
-                            end_date: '2016-09-10 18:00'
-                        },
-                        {
-                            id: 'fLKd67zPkj',
-                            start_date: '2016-09-12 08:00',
-                            end_date: '2016-09-21 18:00'
-                        }
-                    ]
-                }
-            ]
-        });
+            resizeTask: false,
+            tasks: taskies,
+            users: usies,
+//            tasks: [{id: '0'}],
+//            users: [{   name: '0',
+//                        group: 'Rooms',
+//                        tasks: []
+//                    },
+//            ]
+        }
+//        console.log("taskies en usies");
+//        console.log("Taskies: " + tasken.length + " usies: " + usen.length );
+//        for (i = 0; i < tasken.length; i++) {
+//            console.log(tasken[i]);
+//            pitObj.tasks.push(tasken[i]);
+//        }
+//
+//        for (i = 0; i < usen.length; i++) {
+//            console.log(usen[i]);
+//            pitObj.users.push(usen[i]);
+//        }
 
-        var MyFunctionResult = $().GetTasks();
 
-        console.log(MyFunctionResult);
+        //pitObj.tasks = MyFunctionResult;
+        console.log("Pitobj tasks");
+        console.log(pitObj.tasks);
+        console.log(pitObj.users);
 
-    });
+        $("#pit-scheduler").pitScheduler( pitObj );
+
+
+        //var MyFunctionResult = $().GetTasks();
+
+        //console.log(MyFunctionResult);
+
+};
 
 
