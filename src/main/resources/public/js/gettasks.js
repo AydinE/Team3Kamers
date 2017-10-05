90cdf9a2a2aa89fc9dac47fdf78b95824809fc49
Aydin Erdas <erdas001@gmail.com>
2017-10-05 16:22:25 +0200

    Fixed everything

--- src/main/resources/public/js/gettasks.js
+++ src/main/resources/public/js/gettasks.js
@@ -1,15 +1,24 @@
 jQuery.fn.GetTasks = function() {
 
-    id: '1',
-    start_date: '2015-09-10 00:00',
-    end_date: '2016-09-20 00:00'
-
     var tasks = [];
 
     var url = "/api/getAllBookings";
     $.get(url, function(data) {
-            console.log(data);
-            console.log(data[0].room.sizeOfRoom);
+
+            for (i = 0; i < data.length; i++) {
+
+                var obj = {
+                    id: data[i].bookingNr,
+                    name: data[i].guest.firstName + " " + data[i].guest.lastName,
+                    description: data[i].startDate + " " + data[i].endDate,
+                    color: '#536DFE'
+                }
+
+                tasks.push(obj);
+            }
+
     });
+
+    return tasks;
 
 };
--- src/main/resources/public/js/schedulerinit.js
+++ src/main/resources/public/js/schedulerinit.js
@@ -1,162 +1,170 @@
- $(document).ready(function () {
-	$("#pit-scheduler").pitScheduler({
-		locale: 'en',
-		defaultDisplay: 'months',
-		hideEmptyLines: true,
-		disableLabelsMovement: false,
-		defaultGroupName: 'Default group',
-		defaultDate: '2017-09-01 16:30',
-		disableNotifications: false,
-		notificationDuration: 4000,
-		hideSpinner: true,
-		onChange: '',
-		onTaskCreation: '',
-		onUserCreation: '',
-		onTaskRemoval: '',
-		onUserRemoval: '',
-		onUserEdition: '',
-		onTaskAssignation: '',
-		onUserTaskDeletion: '',
-		onTaskEdition: '',
-		disableUndo: false,
-		resizeTask: true,
-		tasks:  [
-			{
-				id: '1',
-				name: 'Jan Janssen',
-				description: '',
-				color: '#50d371',
-			},
-			{
-				id: '2',
-				name: 'Berta Bertus',
-				color: '#50d371'
-			},
-			{
-				id: '3',
-				name: 'Juan Percent',
-				color: '#50d371'
-			},
-			{
-				id: '4',
-				name: 'Charlie Sheen',
-				color: '#50d371',
-			}
-		],
-		users: [
-			   {
-				name: '1',
-				group: 'Rooms',
-				tasks: [
-					{
-						id: '1',
-						start_date: '2015-09-10 00:00',
-						end_date: '2016-09-20 00:00'
-					},
-					{
-						id: '2',
-						start_date: '2016-09-28 6:00',
-						end_date: '2017-09-28 18:30'
-					},
-					{
-						id: '3',
-						start_date: '2016-09-08 10:40',
-						end_date: '2016-09-21 16:00'
-					},
-					{
-						id: '4',
-						start_date: '2016-09-23 10:10',
-						end_date: '2016-09-29 16:50'
-					},
-					{
-						id: '1',
-						start_date: '2016-09-10 08:00',
-						end_date: '2016-09-12 20:00'
-					}
-				]
-			},
-			{
-				name: '2',
-				group: 'Rooms',
-				tasks: [
-					{
-						id: '2',
-						start_date: '2016-09-01 06:00',
-						end_date: '2016-09-04 00:00'
-					},
-					{
-						id: '2',
-						start_date: '2016-09-04 11:00',
-						end_date: '2016-09-08 18:00'
-					},
-					{
-						id: '3',
-						start_date: '2016-09-07 17:00',
-						end_date: '2016-09-11 20:00'
-					}
-				]
-			},
-			{
-				name: '3',
-				group: 'Rooms',
-				tasks: [
-					{
-						id: '2',
-						start_date: '2017-09-01 02:30',
-						end_date: '2017-09-03 09:00'
-					},
-					{
-						id: '2',
-						start_date: '2017-09-04 17:30',
-						end_date: '2017-09-24 07:42'
-					},
-					{
-						id: '4',
-						start_date: '2017-09-02 17:30',
-						end_date: '2017-09-11 07:42'
-					},
-					{
-						id: '1',
-						start_date: '2017-09-13 10:00',
-						end_date: '2017-09-26 16:00'
-					},
-					{
-						id: '2',
-						start_date: '2017-09-30 10:00',
-						end_date: '2017-10-05 16:00'
-					},
-					{
-						id: '1',
-						start_date: '2017-10-23 10:00',
-						end_date: '2017-11-04 16:00'
-					},
-					{
-						id: '3',
-						start_date: '2017-11-06 10:00',
-						end_date: '2017-11-09 16:00'
-					},
-					{
-						id: '4',
-						start_date: '2017-11-10 10:00',
-						end_date: '2017-11-21 16:00'
-					},
-					{
-						id: '1',
-						start_date: '2017-09-01 08:00',
-						end_date: '2017-09-10 18:00'
-					},
-					{
-						id: '2',
-						start_date: '2017-09-12 08:00',
-						end_date: '2017-09-21 18:00'
-					}
-				]
-			}
-		]
-	});
+$(document).ready(function () {
+        $("#pit-scheduler").pitScheduler({
+            locale: 'en',
+            defaultDisplay: 'months',
+            hideEmptyLines: true,
+            disableLabelsMovement: false,
+            defaultGroupName: 'Default group',
+            defaultDate: '2016-09-01 16:30',
+            disableNotifications: false,
+            notificationDuration: 4000,
+            hideSpinner: true,
+            onChange: '',
+            onTaskCreation: '',
+            onUserCreation: '',
+            onTaskRemoval: '',
+            onUserRemoval: '',
+            onUserEdition: '',
+            onTaskAssignation: '',
+            onUserTaskDeletion: '',
+            onTaskEdition: '',
+            disableUndo: false,
+            resizeTask: true,
+            tasks:  [
+                {
+                    id: 'djJ3d7sjk928S0',
+                    name: 'Jacuzzi access',
+                    description: 'Allows users to access to the jacuzzi',
+                    color: '#fcd720',
+                    tag: 'limited places',
+                    tagColor: '#50d371'
+                },
+                {
+                    id: 'fJD67Bd4jh7',
+                    name: 'SPA access',
+                    description: 'Allows usesr to access to the SPA',
+                    color: '#E91E63'
+                },
+                {
+                    id: 'dj8aA9nL0Su7',
+                    name: 'Internet access',
+                    description: 'Allows users to access to internet',
+                    color: '#536DFE'
+                },
+                {
+                    id: 'fLKd67zPkj',
+                    name: 'VIP services',
+                    color: '#50d371',
+                    tag: 'Only VIPs',
+                    tagColor: '#d200a5'
+                }
+            ],
+            users: [
+                   {
+                    name: 'Ai WeiWei',
+                    group: 'Group A',
+                    tasks: [
+                        {
+                            id: 'djJ3d7sjk928S0',
+                            start_date: '2015-09-10 00:00',
+                            end_date: '2016-09-20 00:00'
+                        },
+                        {
+                            id: 'djJ3d7sjk928S0',
+                            start_date: '2016-09-28 6:00',
+                            end_date: '2017-09-28 18:30'
+                        },
+                        {
+                            id: 'fJD67Bd4jh7',
+                            start_date: '2016-09-08 10:40',
+                            end_date: '2016-09-21 16:00'
+                        },
+                        {
+                            id: 'fJD67Bd4jh7',
+                            start_date: '2016-09-23 10:10',
+                            end_date: '2016-09-29 16:50'
+                        },
+                        {
+                            id: 'fLKd67zPkj',
+                            start_date: '2016-09-10 08:00',
+                            end_date: '2016-09-12 20:00'
+                        }
+                    ]
+                },
+                {
+                    name: 'Michel Petrucciani',
+                    group: 'Group A',
+                    tasks: [
+                        {
+                            id: 'djJ3d7sjk928S0',
+                            start_date: '2016-09-01 06:00',
+                            end_date: '2016-09-04 00:00'
+                        },
+                        {
+                            id: 'djJ3d7sjk928S0',
+                            start_date: '2016-09-04 11:00',
+                            end_date: '2016-09-08 18:00'
+                        },
+                        {
+                            id: 'fLKd67zPkj',
+                            start_date: '2016-09-07 17:00',
+                            end_date: '2016-09-11 20:00'
+                        }
+                    ]
+                },
+                {
+                    name: 'Jimi Hendrix',
+                    group: 'Group B',
+                    tasks: [
+                        {
+                            id: 'djJ3d7sjk928S0',
+                            start_date: '2016-09-01 02:30',
+                            end_date: '2016-09-03 09:00'
+                        },
+                        {
+                            id: 'djJ3d7sjk928S0',
+                            start_date: '2016-09-04 17:30',
+                            end_date: '2016-09-24 07:42'
+                        },
+                        {
+                            id: 'dj8aA9nL0Su7',
+                            start_date: '2016-09-02 17:30',
+                            end_date: '2016-09-11 07:42'
+                        },
+                        {
+                            id: 'fJD67Bd4jh7',
+                            start_date: '2016-09-13 10:00',
+                            end_date: '2016-09-26 16:00'
+                        },
+                        {
+                            id: 'fJD67Bd4jh7',
+                            start_date: '2016-09-30 10:00',
+                            end_date: '2016-10-05 16:00'
+                        },
+                        {
+                            id: 'fJD67Bd4jh7',
+                            start_date: '2016-10-23 10:00',
+                            end_date: '2016-11-04 16:00'
+                        },
+                        {
+                            id: 'fJD67Bd4jh7',
+                            start_date: '2016-11-06 10:00',
+                            end_date: '2016-11-09 16:00'
+                        },
+                        {
+                            id: 'fJD67Bd4jh7',
+                            start_date: '2016-11-10 10:00',
+                            end_date: '2016-11-21 16:00'
+                        },
+                        {
+                            id: 'fLKd67zPkj',
+                            start_date: '2016-09-01 08:00',
+                            end_date: '2016-09-10 18:00'
+                        },
+                        {
+                            id: 'fLKd67zPkj',
+                            start_date: '2016-09-12 08:00',
+                            end_date: '2016-09-21 18:00'
+                        }
+                    ]
+                }
+            ]
+        });
 
-	var MyFunctionResult = $().GetTasks();
+        var MyFunctionResult = $().GetTasks();
 
-	console.log(MyFunctionResult);
+        console.log(MyFunctionResult);
 
-});
+    });
+
+
